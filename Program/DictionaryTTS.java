572717cf36c1a474258a5e423167256bb94d1767
K-Minsu <79684170+K-Minsu@users.noreply.github.com>
2021-06-06 21:24:50 +0900

    Update DictionaryTTS.java

--- Program/DictionaryTTS.java
+++ Program/DictionaryTTS.java
@@ -1,3 +1,34 @@
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
 public class DictionaryTTS {
-  
+	private String name;
+	private com.sun.speech.freetts.Voice voice;
+	
+	public DictionaryTTS(String name) {
+		this.name = name;
+		this.voice = VoiceManager.getInstance().getVoice(this.name);
+		
+		if(this.voice != null) {
+			this.voice.allocate();
+		}
+		else {
+			throw new IllegalStateException("Cannot find voice : " + this.voice);
+		}
+	}
+	
+	public void say(String something) {
+		try {
+			this.voice.speak(something);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sayMultiple(String[] something) {
+		for(int i=0; i<something.length; i++) {
+			this.say(something[i]);
+		}
+	}
 }
