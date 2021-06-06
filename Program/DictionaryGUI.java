c3e911c0d3b2b48f56661983f1c0c62b9d88aa3b
K-Minsu <79684170+K-Minsu@users.noreply.github.com>
2021-06-06 21:25:08 +0900

    Update DictionaryGUI.java

--- Program/DictionaryGUI.java
+++ Program/DictionaryGUI.java
@@ -1,3 +1,10 @@
-public class DictionaryGUI {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
+public class DictionaryGUI extends JFrame {
+	public DictionaryGUI() {
+		this.setTitle("영어사전");
+		this.setSize(500, 500);
+	}
 }
